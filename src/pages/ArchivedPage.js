eefad4db914ed4fe9c28a8ad99be18c8c8c230e7
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2023-01-02 10:47:34 +0700

    menambah fitur loading

--- src/pages/ArchivedPage.js
+++ src/pages/ArchivedPage.js
@@ -1,51 +1,64 @@
-import React from 'react';
-import {getArchivedNotes,deleteNote,unarchiveNote} from '../utils/network';
-import { useSearchParams } from 'react-router-dom';
-import CardList from '../component/CardList'; 
-import SearchBar from '../component/SearchBar';
+import React from "react";
+import { getArchivedNotes, deleteNote, unarchiveNote } from "../utils/network";
+import { useSearchParams } from "react-router-dom";
+import CardList from "../component/CardList";
+import SearchBar from "../component/SearchBar";
 // import PropTypes from 'prop-types';
-import LocaleContext from '../context/LocaleContext';
- 
+import LocaleContext from "../context/LocaleContext";
+
 function HomePage() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [archived_notes, setNotes] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
   const [keyword, setKeyword] = React.useState(() => {
-    return searchParams.get('keyword') || ''
+    return searchParams.get("keyword") || "";
   });
   const { locale } = React.useContext(LocaleContext);
   React.useEffect(() => {
     getArchivedNotes().then(({ data }) => {
       setNotes(data);
+      setTimeout(() => {
+        setLoading(true);
+      }, 400);
     });
   }, []);
   async function onDeleteHandler(id) {
     await deleteNote(id);
     const { data } = await getArchivedNotes();
     setNotes(data);
+    setTimeout(() => {
+      setLoading(true);
+    }, 400);
   }
   async function ononUnArchivedHandler(id) {
     await unarchiveNote(id);
     const { data } = await getArchivedNotes();
     setNotes(data);
+    setTimeout(() => {
+      setLoading(true);
+    }, 400);
   }
   function onKeywordChangeHandler(keyword) {
     setKeyword(keyword);
     setSearchParams({ keyword });
   }
   const filteredNotes = archived_notes.filter((note) => {
-    return note.title.toLowerCase().includes(
-      keyword.toLowerCase()
-    );
+    return note.title.toLowerCase().includes(keyword.toLowerCase());
   });
-  if(archived_notes.length === 0){
-    return <h1>Data Loading</h1>
-}
-  return (
+  return loading === true ? (
     <section>
       <SearchBar keyword={keyword} keywordChange={onKeywordChangeHandler} />
-      <h2>{locale === 'id' ? 'Daftar Catatan Arsip' : 'Archived Notes List'}</h2>
-      <CardList notes={filteredNotes} onUnArchived={ononUnArchivedHandler} onDelete={onDeleteHandler} />
+      <h2>
+        {locale === "id" ? "Daftar Catatan Arsip" : "Archived Notes List"}
+      </h2>
+      <CardList
+        notes={filteredNotes}
+        onUnArchived={ononUnArchivedHandler}
+        onDelete={onDeleteHandler}
+      />
     </section>
-  )
+  ) : (
+    <h2>Data Loading!</h2>
+  );
 }
 export default HomePage;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,47 +1,61 @@
-import React from 'react';
-import { useSearchParams } from 'react-router-dom';
-import {getActiveNotes,archiveNote,deleteNote} from '../utils/network';
-import CardList from '../component/CardList'
-import SearchBar from '../component/SearchBar';
-import LocaleContext from '../context/LocaleContext';
- 
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { getActiveNotes, archiveNote, deleteNote } from "../utils/network";
+import CardList from "../component/CardList";
+import SearchBar from "../component/SearchBar";
+import LocaleContext from "../context/LocaleContext";
+
 function HomePage() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [active_notes, setNotes] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
   const [keyword, setKeyword] = React.useState(() => {
-    return searchParams.get('keyword') || ''
+    return searchParams.get("keyword") || "";
   });
   const { locale } = React.useContext(LocaleContext);
   React.useEffect(() => {
     getActiveNotes().then(({ data }) => {
       setNotes(data);
+      setTimeout(() => {
+        setLoading(true);
+      }, 400);
     });
   }, []);
   async function onDeleteHandler(id) {
     await deleteNote(id);
     const { data } = await getActiveNotes();
     setNotes(data);
+    setTimeout(() => {
+      setLoading(true);
+    }, 400);
   }
   async function onArchivedHandler(id) {
     await archiveNote(id);
     const { data } = await getActiveNotes();
     setNotes(data);
+    setTimeout(() => {
+      setLoading(true);
+    }, 400);
   }
   function onKeywordChangeHandler(keyword) {
     setKeyword(keyword);
     setSearchParams({ keyword });
   }
   const filteredNotes = active_notes.filter((note) => {
-    return note.title.toLowerCase().includes(
-      keyword.toLowerCase()
-    );
+    return note.title.toLowerCase().includes(keyword.toLowerCase());
   });
-  return (
+  return loading === true ? (
     <section>
       <SearchBar keyword={keyword} keywordChange={onKeywordChangeHandler} />
-      <h2>{locale === 'id' ? 'Daftar Catatan' : 'Notes List'}</h2>
-      <CardList notes={filteredNotes} onArchived={onArchivedHandler} onDelete={onDeleteHandler} />
+      <h2>{locale === "id" ? "Daftar Catatan" : "Notes List"}</h2>
+      <CardList
+        notes={filteredNotes}
+        onArchived={onArchivedHandler}
+        onDelete={onDeleteHandler}
+      />
     </section>
-  )
+  ) : (
+    <h2>Data Loading</h2>
+  );
 }
 export default HomePage;
