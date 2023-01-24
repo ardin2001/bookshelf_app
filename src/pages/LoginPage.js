ac2226784558d6b41eced4a4b780b54f01d6f3b2
ardinnugraha2001@gmail.com <ardinnugraha2001@gmail.com>
2023-01-24 21:28:34 +0700

    memperbarui halaman login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -9,6 +9,7 @@ function LoginPage({ loginSuccess }) {
   return (
     <section className='login-page'>
       <h2>{locale === 'id' ? 'Silakan Masuk Untuk Melanjutkan' : "Please Login To Continue"}</h2>
+      <p style={{color : 'red', textAlign : 'center', paddingTop : '10px'}}>{locale === 'id' ? 'Harap gunakan perangkat desktop karena website tidak responsive' : "Please use a desktop device because the website is not responsive"}</p>
       <LoginNew loginSuccess={loginSuccess}/>
       <p className='note'>{locale === 'id' ? 'Belum punya akun? ' : "Don't have an account yet? "}
         <Link to="/register">{locale === 'id' ? 'Daftar di sini.' : "Register here."}</Link>
